fb687986beca09ed3151b3c6ddc819844c847b54
Andrew Cummings <agcummings@gmail.com>
2025-03-15 00:00:27 -0700

    fast interp w/ command line args

--- interp.py
+++ interp.py
@@ -1,19 +1,6 @@
 import numpy as np
 from scipy.io import wavfile
-from matplotlib import pyplot as plt
-
-semitone = 0.06
-newRate = 1 - 0.5*semitone
-
-startHold = 1.5
-endHold = 1.5
-
-padding = 512
-halfFadeSize = 500
-fadeInBefore = np.linspace(0, 0.5, halfFadeSize, endpoint=False)
-fadeInAfter = np.linspace(0.5, 1, halfFadeSize, endpoint=False)
-fadeOutBefore = np.linspace(1, 0.5, halfFadeSize, endpoint=False)
-fadeOutAfter = np.linspace(0.5, 0, halfFadeSize, endpoint=False)
+import argparse
 
 def integersWithin(a, b):
     """
@@ -73,23 +60,38 @@ def upsampleCurve(startK, endK, startOffset, endOffset, length, sampleRate):
 
     outU = np.concatenate([startU, midU, endU])
     print("Generated", outU.shape[0], "sample times:", outU)
-    print("Start to mid", (midU[0] - startU[-1]))
-    print("Mid to end", (endU[0] - midU[-1]))
+    if startU.shape[0] > 0:
+        print("Start to mid", (midU[0] - startU[-1]))
+    if endU.shape[0] > 0:
+        print("Mid to end", (endU[0] - midU[-1]))
     print("Durations =", startHoldSamples, curveSamples, endHoldSamples)
     print("Counts =", startU.shape[0], midU.shape[0], endU.shape[0])
 
     return outU
 
-def fast_sinc_interp(x, s, u):
+def fast_sinc_interp(x, s, u, window_size=1023):
     """
-    1. Find the indecies of s which are closest to the values in u (i.e. u[0] is closest to s[0], u[10] is closest to s[6])
-    2. Extract the N surrounding values in s and u to s_wind and u_wind
-    3. Extract the N surrounding values around each s index from x into x_wind
-    4. Perform x_wind * sinc((u_wind-s_wind)/(s[1]-s[0]))
+    Uses sinc interpolation, but only uses samples in a window around the target sample.
+    Args:
+        x (NDArray): Signal to be interpolated.
+        s (NDArray): Time points of x.
+        u (NDArray): Time points to upsample to.
+        window_size (int): Size of window around each target sample. Must be odd, default is 1023.
+    Returns:
+        NDArray: Upsampled signal
     """
 
-    window_size = 999 # must be odd
-    
+    """
+    1. Find the indecies of s which are closest to the values in u (i.e. u[0] is closest to s[0], u[10] is closest to s[6])
+    2. Extract the N surrounding values in s and x to x_wind and s_wind
+    3. Perform x_wind * sinc((u-s_wind)/(s[1]-s[0]))
+    """
+
+    assert (window_size%2 == 1)
+    assert (x.shape[0] == s.shape[0])
+
+    print("Fast interp", s.shape[0], "=>", u.shape[0], ", window size =", window_size)
+
     s_i = 0
     s_indecies = np.empty(u.shape[0], dtype=np.int32)
     for u_i in range(u.shape[0]):
@@ -98,24 +100,25 @@ def fast_sinc_interp(x, s, u):
 
         s_indecies[u_i] = s_i
 
+
     delta = s[1] - s[0]
     x = np.pad(x, (window_size//2, window_size//2), constant_values=(0,0))
     s = np.pad(s, (window_size//2, window_size//2), constant_values=(0,0))
-    u = np.pad(u, (window_size//2, window_size//2), constant_values=(0,0))
 
-    x_wind = np.lib.stride_tricks.sliding_window_view(u, window_size)[s_indecies]
+    x_wind = np.lib.stride_tricks.sliding_window_view(x, window_size)[s_indecies]
     s_wind = np.lib.stride_tricks.sliding_window_view(s, window_size)[s_indecies]
-    u_wind = np.lib.stride_tricks.sliding_window_view(u, window_size)
 
-    dt = (u_wind-s_wind)/delta
-    plt.plot(dt[:,0])
-    plt.show()
-
+    dt = (u[:,None]-s_wind)/delta
+    print("...dt")
     sinc_ = np.sinc(dt)
-    return np.sum(x_wind*sinc_, axis=1)
+    print("...sinc, error less than", np.max(sinc_[window_size//2:-window_size//2,0]))
+    interpolated = np.sum(x_wind*sinc_, axis=1)
+    print("...dot product, finished")
+    return interpolated
 
 def sinc_interpolation(x, s, u):
-    """Whittaker–Shannon or sinc or bandlimited interpolation.
+    """
+    Sinc interpolation.
     Args:
         x (NDArray): signal to be interpolated, can be 1D or 2D
         s (NDArray): time points of x (*s* for *samples*) 
@@ -125,114 +128,44 @@ def sinc_interpolation(x, s, u):
     Reference:
         This code is based on https://gist.github.com/endolith/1297227
         and the comments therein.
-    TODO:
-        * implement FFT based interpolation for speed up
     """
     sinc_ = np.sinc((u - s[:, None])/(s[1]-s[0]))
     return np.dot(x, sinc_)
 
-def main():
-    samplerate, data = wavfile.read('tts_speedsct_nolmt.wav')
-    print("Loaded", samplerate, data.shape, data.dtype, "... New rate of", newRate)
+def main(inFile, outFile, startRate, endRate, startHold, endHold, useSlow):
+    samplerate, data = wavfile.read(inFile)
+    print("Loaded", samplerate, data.shape, data.dtype, "... rate from", startRate, "to", endRate)
 
-    count = 0
-    size = samplerate // 4
-    maxCount = data.shape[0] // size
-    print("Count", maxCount, "differs by", (data.shape[0] - maxCount*size))
+    length = data.shape[0]
 
-    s = np.arange(0, size*maxCount) * (1 / samplerate)
-    u = upsampleCurve(newRate, 1.0, startHold, endHold, s.shape[0], samplerate)
-    outData = np.zeros((u.shape[0], 2), dtype=np.int32)
+    s = np.arange(0, length) * (1 / samplerate)
+    u = upsampleCurve(startRate, endRate, startHold, endHold, length, samplerate)
 
-    fast_sinc_interp(data,s,u)
-    exit()
-    
-    while (count < maxCount):
-        nonPadStart = count*size
+    if useSlow:
+        interpFunc = sinc_interpolation
+    else:
+        interpFunc = fast_sinc_interp
 
-        if count == 0:
-            sSize = size + padding # pad right side
-
-            startSIndex = nonPadStart
-            endSIndex = startSIndex + sSize
-
-            startUIndex = np.searchsorted(u, s[startSIndex])
-            endUIndex = np.searchsorted(u, s[endSIndex])
-
-            finalUSize = np.searchsorted(u, s[nonPadStart + size]) - startUIndex
-            startUPadding = 0
-
-        elif count < maxCount-1:
-            sSize = size + padding*2 # pad both sides
-
-            startSIndex = nonPadStart - padding
-            endSIndex = startSIndex + sSize
-
-            startUIndex = np.searchsorted(u, s[startSIndex])
-            endUIndex = np.searchsorted(u, s[endSIndex])
-
-            finalUSize = np.searchsorted(u, s[nonPadStart + size]) - np.searchsorted(u, s[nonPadStart])
-            startUPadding = np.searchsorted(u, s[nonPadStart]) - np.searchsorted(u, s[startSIndex]) # difference between the neg padded start and the real start
-
-        else:
-            sSize = size + padding # pad left side
-
-            startSIndex = nonPadStart - padding
-            endSIndex = startSIndex + sSize
-            assert endSIndex == s.shape[0]
-
-            startUIndex = np.searchsorted(u, s[startSIndex])
-            endUIndex = u.shape[0]
-
-            finalUSize = endUIndex - np.searchsorted(u, s[nonPadStart])
-            startUPadding = np.searchsorted(u, s[nonPadStart]) - np.searchsorted(u, s[startSIndex])
-
+    outData = np.empty((u.shape[0], 2), dtype=np.int32)
+    outData[:,0] = interpFunc(data[:,0], s, u).astype(np.int32)
+    outData[:,1] = interpFunc(data[:,1], s, u).astype(np.int32)
         
-        print("s:", startSIndex, endSIndex)
-        print("u:", startUIndex, endUIndex)
-        print("output trim:", startUPadding, finalUSize)
-
-        left = data[startSIndex:endSIndex, 0]
-        right = data[startSIndex:endSIndex, 1]
-
-        outL = sinc_interpolation(left, s[startSIndex:endSIndex], u[startUIndex:endUIndex])
-        outR = sinc_interpolation(right, s[startSIndex:endSIndex], u[startUIndex:endUIndex])
-
-        fadeStOff = 0
-        fadeNdOff = 0
-        outI = startUIndex+startUPadding
-
-        if startUPadding > 0 and halfFadeSize > 0:
-            print("Fade in")
-            fadeStOff = halfFadeSize
-
-            outL[startUPadding-halfFadeSize:startUPadding] *= fadeInBefore
-            outL[startUPadding:startUPadding+halfFadeSize] *= fadeInAfter
-            
-            outR[startUPadding-halfFadeSize:startUPadding] *= fadeInBefore
-            outR[startUPadding:startUPadding+halfFadeSize] *= fadeInAfter
-
-        if outI+finalUSize < outData.shape[0] and halfFadeSize > 0:
-            print("Fade out")
-            fadeNdOff = halfFadeSize
-
-            outL[startUPadding+finalUSize-halfFadeSize:startUPadding+finalUSize] *= fadeOutBefore
-            print(startUPadding+finalUSize, startUPadding+finalUSize+halfFadeSize)
-            print(outL.shape)
-            outL[startUPadding+finalUSize:startUPadding+finalUSize+halfFadeSize] *= fadeOutAfter
-            
-            outR[startUPadding+finalUSize-halfFadeSize:startUPadding+finalUSize] *= fadeOutBefore
-            outR[startUPadding+finalUSize:startUPadding+finalUSize+halfFadeSize] *= fadeOutAfter
-
-        
-        outData[outI-fadeStOff:outI+finalUSize+fadeNdOff, 0] += outL[startUPadding-fadeStOff:startUPadding+finalUSize+fadeNdOff].astype(np.int32)
-        outData[outI-fadeStOff:outI+finalUSize+fadeNdOff, 1] += outR[startUPadding-fadeStOff:startUPadding+finalUSize+fadeNdOff].astype(np.int32)
-
-        print("Output", count, startUIndex+startUPadding)
-        count += 1
-        
-    wavfile.write('tts_speedsct_nolmt_combo.wav', samplerate, outData)
-    print("Wrote output data.")
+    wavfile.write(outFile, samplerate, outData.astype(np.int32))
+    print("Wrote output data to", outFile)
 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser(
+        prog="Sinc Interpolation",
+        description="Changes the sample rate of a signal using sinc interpolation.")
+
+    parser.add_argument("-i", "--input", help="Input file name.", required=True)
+    parser.add_argument("-s", "--start", help="Starting sample rate, as a ratio to the original sample rate.", default=1.0, type=float)
+    parser.add_argument("-e", "--end", help="Ending sample rate, as a ratio to the original sample rate.", default=1.0, type=float)
+    parser.add_argument("-d", "--delay", help="Time in seconds to hold at the starting sample rate before beginning to change.", default=0.0, type=float)
+    parser.add_argument("-H", "--hold", help="Time in seconds to hold at the ending sample rate once reached.", default=0.0, type=float)
+    parser.add_argument("-S", "--slow", action="store_true", help="Use full sinc interpolation without windowing (not recommended).")
+    parser.add_argument("output", help="Output file name.")
+
+    args = parser.parse_args()
+
+    main(args.input, args.output, args.start, args.end, args.delay, args.hold, args.slow)
